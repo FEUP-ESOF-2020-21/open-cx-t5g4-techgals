e417c8851a770a66e43a2d1bf80bb4c0c302730b
alexmsf <xana.ferreira2000@hotmail.com>
2020-12-08 20:45:17 +0000

    interests can now be eliminated and added directly from the profile page!

--- proj_src/lib/Screens/Profile/screen_profile.dart
+++ proj_src/lib/Screens/Profile/screen_profile.dart
@@ -26,12 +26,12 @@ class _ProfileState extends State<Profile> {
   String _interestsString = '';
   QuerySnapshot _userQS;
   String _newInterest = "";
-  Stream<DocumentSnapshot> _stream;
 
   @override
   void initState() {
     super.initState();
     _getInfo();
+    _getInterests();
   }
 
   _getInfo() async {
@@ -45,6 +45,8 @@ class _ProfileState extends State<Profile> {
         _email = value;
       });
     });
+  }
+  _getInterests() async {
     await DatabaseMethods().getUser(_userName).then((value) {
       setState(() {
         _userQS = value;
@@ -56,32 +58,6 @@ class _ProfileState extends State<Profile> {
     }
     _interests.forEach((element) {element.toLowerCase();});
     _interestsString = _interests.join(" / ");
-
-    DatabaseMethods().getUserInterests(_user.uid).then((val) {
-      setState(() {
-        _stream = val;
-      });
-    });
-  }
-
-  Widget _listeInterets(){
-    return StreamBuilder(
-      stream: _stream,
-      builder: (context, snapshot){
-        return snapshot.hasData ?  ListView.builder(
-          itemCount: _interests.length,//snapshot.data.documents.length,
-          itemBuilder: (context, index){
-            print(_interests[index]);
-            return InterestTile(
-              message: _interests[index],//snapshot.data.documents[index]['message'],
-            );
-          },
-          scrollDirection: Axis.vertical,
-        )
-            :
-        Container();
-      },
-    );
   }
 
   @override
@@ -92,7 +68,7 @@ class _ProfileState extends State<Profile> {
           padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
           child: Stack(
             children: <Widget> [
-              _test(),
+              _interestsList(),
               //addTile("add a new interest!"),
               //_listeInterets(),
               /*Container(
@@ -153,7 +129,7 @@ class _ProfileState extends State<Profile> {
     );
   }
 
-  Widget _test() {
+  Widget _interestsList() {
     return ListView.builder(
       itemCount: (_interests.length + 4),//snapshot.data.documents.length,
       itemBuilder: (context, index){
@@ -164,10 +140,7 @@ class _ProfileState extends State<Profile> {
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           );
         }
-        else if(index == 1) {
-          return
-            Divider(height: 30,);
-        }
+        else if(index == 1) { return Divider(height: 30,); }
         else if(index == 2) {
           return
             TextField(
@@ -177,9 +150,10 @@ class _ProfileState extends State<Profile> {
                   icon: Icon(Icons.add_circle_outline_sharp, color: k2PrimaryColor, size: 25,),
                   padding: EdgeInsets.only(right: 5),
                   onPressed: () async {
-                    if(!_interests.contains(_newInterest.toLowerCase()) && _newInterest != null) {
+                    if(!_interests.contains(_newInterest.toLowerCase()) && _newInterest != null && _newInterest != "") {
                       await HelperFunctions.getUserNameSharedPreference().then((val) {
                         DatabaseMethods(uid: _user.uid).addInterest(_newInterest);
+                        _getInterests();
                       });
                     }
                   },
@@ -191,130 +165,57 @@ class _ProfileState extends State<Profile> {
               },
             );
         }
-        else if(index == 3) {
-          return
-            Divider(height: 30,);
-        }
+        else if(index == 3) { return Divider(height: 30,); }
         else {
-          return InterestTile(
-            message: _interests[index-4],
-          );
+          return _interestTile(_interests[index-4]);
         }
       },
       scrollDirection: Axis.vertical,
     );
   }
 
-
-
-  void _seeInterests(BuildContext context) {
-    Widget cancelButton = FlatButton(
-      child: Text("Cancel"),
-      onPressed:  () {
-        Navigator.of(context).pop();
-      },
-    );
-    Widget addButton = FlatButton(
-      child: Text("Add"),
-      onPressed:  () async {
-        if(_interests.contains(_newInterest.toLowerCase())) {
-          Navigator.of(context).pop();
-        }
-        else if (_newInterest != null){
-          await HelperFunctions.getUserNameSharedPreference().then((val) {
-            DatabaseMethods(uid: _user.uid).addInterest(_newInterest);
-          });
-          //_getInfo();
-          Navigator.of(context).pop();
-        }
-      },
-    );
-
-    AlertDialog alert = AlertDialog(
-      title: Text("Interests"),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          _interests.isEmpty ?
-          Text('You don\'t have any interests yet... add some!')
-              : Text(_interestsString),
-          TextField(
-              decoration: inputDeco("new interest"),
-              onChanged: (val) {
-                _newInterest = val;
-              },
-              style: TextStyle(
-                  fontSize: 15.0,
-                  height: 2.0,
-                  color: Colors.black
-              )
-          ),
-        ],
-      ),
-      actions: [
-        cancelButton,
-        addButton,
-      ],
-    );
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alert;
-      },
-    );
-  }
-
-  Widget addTile(String message) {
+  Widget _interestTile(String message) {
     return Container(
-      //padding: EdgeInsets.all(5),
-      //alignment: Alignment.bottomCenter,
-      child:
-        Row(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: message,
-              ),
-            ),
-          /*  TextField(
-              //decoration: inputDeco(message),
-              onChanged: (val) async {
-                _newInterest = val;
-              },
-              style: TextStyle(
-                  fontSize: 17.0,
-                  color: Colors.white)
-            ),
-            GestureDetector(
-              onTap: () async {
-
-              },
-              child: Icon(Icons.add_circle_outline_sharp, color: Colors.white, size: 25,),
-            )*/
-          ],
+      padding: EdgeInsets.symmetric(vertical: 6),
+      alignment: Alignment.center,
+      child: Container(
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.symmetric(vertical: 15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                k2PrimaryColor,
+                kPrimaryColor,
+              ]
+          ),
+          borderRadius: BorderRadius.circular(5),
         ),
+        child:
+        Padding(
+          padding: EdgeInsets.only(left: 15, right: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(message, style: TextStyle(fontSize: 17.0, color: Colors.white)),
+              GestureDetector(
+                onTap: () async {
+                  await HelperFunctions.getUserNameSharedPreference().then((val) {
+                    DatabaseMethods(uid: _user.uid).removeInterest(message);
+                    _getInterests();
+                  });
+                },
+                child: Icon(Icons.remove_circle_outline_rounded, color: Colors.white, size: 25,),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 
-  Widget _listInterests(){
-    return ListView.builder(
-      itemCount: _interests.length,
-      itemBuilder: (context, index){
-   /*     return Container(
-            padding: EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(23),
-              color: k2PrimaryColor,
-            ),
-            child: Text(_interests[index], textAlign: TextAlign.start, style: TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold, color: Colors.black, letterSpacing: -0.5)),
-        );*/
-        return Text(_interests[index], style: TextStyle(fontSize: 17.0),);
-      },
-      scrollDirection: Axis.horizontal,
-    );
-  }
 }
+
+
 
 
 
