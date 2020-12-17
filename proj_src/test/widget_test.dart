85e9cb4aaaea82b36e2678c75813736d220796ef
RaquelFSF <61162979+RaquelFSF@users.noreply.github.com>
2020-12-17 22:04:25 +0000

    done testing

--- proj_src/test/widget_test.dart
+++ proj_src/test/widget_test.dart
@@ -6,29 +6,9 @@
 // tree, read text, and verify that the values of widget properties are correct.
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:proj_src/BackEnd/database.dart';
 import 'package:proj_src/Screens/Initials/welcome.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
 
 void main() {
-  /*testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });*/
-
   testWidgets('Login in welcome page', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(home: Welcome()));
     expect(find.text("LOG IN"), findsNWidgets(1));
@@ -38,25 +18,4 @@ void main() {
     await tester.pumpWidget(MaterialApp(home: Welcome()));
     expect(find.text("SIGN UP"), findsNWidgets(1));
   });
-
-/*  testWidgets('shows messages', (WidgetTester tester) async {
-    // Populate the mock database.
-    final firestore = MockFirestoreInstance();
-    await firestore.collection('users').add({
-      'username': 'Oscar',
-      'interests': ['css'],
-      'email': 'oscar@gmail.com'
-    });
-
-    // Render the widget.
-    await tester.pumpWidget(MaterialApp(
-        title: 'Firestore Example', home: DatabaseMethods()));
-    // Let the snapshots stream fire a snapshot.
-    await tester.idle();
-    // Re-render.
-    await tester.pump();
-    // // Verify the output.
-    expect(find.text('Hello world!'), findsOneWidget);
-    expect(find.text('Message 1 of 1'), findsOneWidget);
-  });*/
 }
